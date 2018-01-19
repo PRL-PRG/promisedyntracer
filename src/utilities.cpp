497bff43387c622ca84ece3e1c83b09771e14bc1
Konrad Siek <konrad.siek@gmail.com>
2018-01-19 14:55:17 +0100

    OpenSSL backwards compatibility

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -206,6 +206,32 @@ const char *get_ns_name(SEXP op) {
     return NULL;
 }
 
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+
+#include <string.h>
+#include <openssl/engine.h>
+
+static void *OPENSSL_zalloc(size_t num) {
+    void *ret = OPENSSL_malloc(num);
+
+    if (ret != NULL)
+        memset(ret, 0, num);
+
+    return ret;
+}
+
+EVP_MD_CTX *EVP_MD_CTX_new(void)
+{
+    return OPENSSL_zalloc(sizeof(EVP_MD_CTX));
+}
+
+void EVP_MD_CTX_free(EVP_MD_CTX *ctx)
+{
+    EVP_MD_CTX_cleanup(ctx);
+    OPENSSL_free(ctx);
+}
+#endif
+
 std::string compute_hash(const char *data) {
     EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
     const EVP_MD *md = EVP_md5();

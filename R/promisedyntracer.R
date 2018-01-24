27836f483def68997509fa1f3a6e25a4ff417ef1
Aviral Goel <goel.aviral@gmail.com>
2018-01-23 21:25:45 -0500

    Fix `dyntrace_promises` function to take `truncate` argument
    This commit fixes `dyntrace_promises` function. It now also
    accepts `truncate` argument. The default value of `verbose`
    argument has been changed to `FALSE`.
    
    This function can be used to resolve #6.

--- R/promisedyntracer.R
+++ R/promisedyntracer.R
@@ -1,4 +1,4 @@
-create_dyntracer <- function(database, truncate=FALSE, verbose=TRUE) {
+create_dyntracer <- function(database, truncate=FALSE, verbose=FALSE) {
   schema <- file.path(path.package("promisedyntracer"), "exec/schema.sql")
   .Call(C_create_dyntracer, database, schema, truncate, verbose)
 }
@@ -6,8 +6,8 @@ create_dyntracer <- function(database, truncate=FALSE, verbose=TRUE) {
 destroy_dyntracer <- function(dyntracer)
   invisible(.Call(C_destroy_dyntracer, dyntracer))
 
-dyntrace_promises <- function(expr, database, verbose=TRUE) {
-  dyntracer <- create_dyntracer(database, verbose)
+dyntrace_promises <- function(expr, database, truncate=FALSE, verbose=FALSE) {
+  dyntracer <- create_dyntracer(database, truncate, verbose)
   result <- dyntrace(dyntracer, expr)
   destroy_dyntracer(dyntracer)
   result
